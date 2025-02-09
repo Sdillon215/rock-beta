2720d5114b9d15b2e27e32e26af1bc8e9ee1e199
Sean_codez <sdillon215@gmail.com>
2025-02-09 14:43:33 -0700

    NavBar logo and placeholder nav links

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,111 @@
+'use client';
+import Image from 'next/image';
+
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+
+type NavigationItem = {
+  name: string;
+  href: string;
+  current?: boolean;
+};
+
+type User = {
+  name: string;
+  email: string;
+  imageUrl: string;
+};
+
+const user: User = {
+  name: 'Tom Cook',
+  email: 'tom@example.com',
+  imageUrl:
+    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+};
+
+const navigation: NavigationItem[] = [
+  { name: 'Rock Beta', href: '#', current: true },
+  { name: 'Routes', href: '#', current: false },
+  { name: 'Forum', href: '#', current: false },
+];
+
+const userNavigation: NavigationItem[] = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
+];
+
+function classNames(...classes: string[]): string {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function NavBar() {
+  return (
+    <div className="min-h-full">
+      <Disclosure as="nav" className="bg-gray-800">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="flex items-center">
+              <div className="shrink-0">
+                <Image
+                  alt="Your Company"
+                  src="/logo.png"
+                  width={60}
+                  height={36}
+                />
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-6 flex items-baseline space-x-4">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      aria-current={item.current ? 'page' : undefined}
+                      className={classNames(
+                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'rounded-md px-3 py-2 text-sm font-medium',
+                      )}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-4 flex items-center md:ml-6">
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="sr-only">Open user menu</span>
+                      <img alt="" src={user.imageUrl} className="size-8 rounded-full" />
+                    </MenuButton>
+                  </div>
+                  <MenuItems
+                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 focus:outline-none"
+                  >
+                    {userNavigation.map((item) => (
+                      <MenuItem key={item.name}>
+                        {({ active }) => (
+                          <a
+                            href={item.href}
+                            className={classNames(
+                              active ? 'bg-gray-100' : '',
+                              'block px-4 py-2 text-sm text-gray-700'
+                            )}
+                          >
+                            {item.name}
+                          </a>
+                        )}
+                      </MenuItem>
+                    ))}
+                  </MenuItems>
+                </Menu>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Disclosure>
+    </div>
+  );
+}
