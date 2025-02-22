1f9f6083ecb0e3606675a29f81d527c9ad228e6a
Sean_codez <sdillon215@gmail.com>
2025-02-22 15:13:30 -0700

    fix(pages[id]): params to type Promise to fix vercel deployment error

--- src/app/state/[id]/page.tsx
+++ src/app/state/[id]/page.tsx
@@ -18,8 +18,8 @@ export const metadata: Metadata = {
     title: 'State Page',
 };
 
-export default async function Page({ params }: { params: { id: string } }) {
-    const { id } = await params;
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+    const id = (await params).id;
     const stateDetails = await fetchStateDetails(id);
     if (!stateDetails) {
         return notFound();
--- src/app/subarea/[id]/page.tsx
+++ src/app/subarea/[id]/page.tsx
@@ -18,8 +18,8 @@ export const metadata: Metadata = {
     title: 'Subarea Page',
 };
 
-export default async function Page({ params }: { params: { id: string } }) {
-    const { id } = await params;
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+    const id = (await params).id;
     const subareaDetails = await fetchSubareaDetails(id);
     if (!subareaDetails) {
         return notFound();
